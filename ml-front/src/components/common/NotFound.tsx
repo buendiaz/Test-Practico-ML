e572e0bfb383ab8941f447da5197e1e7c0225dbf
Max <max@buendiaz.com>
2021-01-15 22:39:47 -0300

    Agrega not found

--- ml-front/src/components/common/NotFound.tsx
+++ ml-front/src/components/common/NotFound.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 const NotFound = () => {
-  return <div>404!</div>
+  return (
+    <React.Fragment>
+      <Helmet>
+        <title>Página no encontrada - Mercado Libre Challenge </title>
+        <meta name="description" content="Mercado Libre Challenge - 404" />
+      </Helmet>
+      <div className="container">
+        <div className="resultBox">
+          <div className="noResults">
+            Error 404! - Página no encontrada
+            <br />Utiliza el buscador para encontrar tus productos
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  )
 }
 
 export default NotFound;
